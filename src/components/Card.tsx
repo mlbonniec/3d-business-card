618ccb8b67f95d13b3c08e974d933f49a4f19c15
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-11-07 17:34:57 +0100

    🎨 Remove empty lines

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,26 +22,26 @@ export default function Card() {
     function transform(scale: Number = 1, _x: Number | null = x, _y: Number | null = y): void {
       cardCRT.style.transform = `rotateY(${_x}deg) rotateX(${_y}deg) scale(${scale})`;
     }
-    
+
     function setPosition(pageX: Number, pageY: Number): void {
       const { top: topIndent, left: leftIndent, width, height } = containerCRT.getBoundingClientRect();  
-      
+
       x = -((+pageX - leftIndent) - width / 2) / 20;
       y = ((+pageY - topIndent) - height / 2) / 15;
-      
+
       transform(1.05)
     }
-    
+
     function resetPosition(): void {
       cardCRT.style.transition = 'all 0.35s ease';
-      
+
       transform(1, 0, 0);
     }
-    
+
     function stopTransition(): void {
       cardCRT.style.transition = ''
     }
-    
+
     const events: IEvents = {
       mouse: {
         mousemove: (e: MouseEvent) => setPosition(e.pageX, e.pageY),
@@ -56,14 +56,14 @@ export default function Card() {
         touchend: resetPosition,
       },
     }
-    
+
     // TODO: touchcancel event
     // TODO: fix document overflow on touch
     if ('TouchEvent' in window)
       eventAdder(containerCRT, events.touch);
     if ('MouseEvent' in window)
       eventAdder(containerCRT, events.mouse);
-      
+
     return function cleanup() {
       if ('TouchEvent' in window)
         eventRemover(containerCRT, events.touch);
